cc72f012c4acee115128e6120f7960e9f69fa3e7
bhadreshce <bhadreshce17@gmail.com>
2023-03-27 09:09:36 +0530

    add

---
+++ lecture/function.js
@@ -0,0 +1,32 @@
+// function Celsius(ferenhit) {
+//    console.log( ((ferenhit-32)*5)/9);
+// }
+
+
+// Celsius(1)
+// Celsius(100)
+
+
+
+// function redius(d) { 
+//     console.log(d/2);
+    
+// }
+// redius(8)
+
+function checkEvenOdd(num) { 
+    if (num % 2 == 0) {
+        console.log('this is an even number');
+    } else { 
+          console.log('this is an odd number');
+    }
+}
+
+checkEvenOdd(7)
+
+
+// _
+//number no
+// alphabetic yes
+//symbol no 
+//$
